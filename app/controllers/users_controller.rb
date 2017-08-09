83ec6d542a096bdd59852f7bdbd77cc66b7712c6
Devan <devan.moylan@backyardlion.com>
2017-08-09 17:29:36 -0600

    Add method 'check_user_rights' to UserController, checked before action of delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  before_action :check_user_rights, only: [:destroy]
 
   def show
   end
@@ -12,9 +13,16 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user.destroy
   end
 
   private
+
+  def check_user_rights
+    if current_user.user?
+      redirect_to root_path
+    end
+  end
 
   def set_user
     @user = User.find(params[:id])
