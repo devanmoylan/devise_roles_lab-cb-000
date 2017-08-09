4d019e8657aadece27c4350b1151b21be46bc0b9
Devan <devan.moylan@backyardlion.com>
2017-08-09 17:51:24 -0600

    Change method check_user_rights to check_if_admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-  before_action :check_user_rights, only: [:destroy]
+  before_action :check_if_admin, only: [:destroy]
 
   def show
   end
@@ -14,14 +14,13 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
+    redirect_to root_path
   end
 
   private
 
-  def check_user_rights
-    if current_user.user?
-      redirect_to root_path
-    end
+  def check_if_admin
+    redirect_to root_path if current_user.user? || current_user.vip?
   end
 
   def set_user
