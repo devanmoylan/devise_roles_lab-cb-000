6b01896f4ff2a4758e0aebeedf4356044b776b1a
Devan <devan.moylan@backyardlion.com>
2017-08-09 17:22:57 -0600

    PostsController Actions: Show, Update, Destroy. Update: only if admin or vip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,32 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
 
+  def show
+  end
+
+  def update
+    if current_user.user?
+      redirect_to root_path
+    else
+      if @post.update(post_params)
+        redirect_to user_path(current_user)
+      else
+        redirect_to root_path
+      end
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
 
 end
